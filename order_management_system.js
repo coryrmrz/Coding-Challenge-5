6f7f3174506a50768c519416fb8583f4d2dcfcaf
coryrmrz <coryramirez2018@gmail.com>
2024-09-20 15:47:03 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,10 @@
+//Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+    { name: 'Espresso', price: 3, quantity: 10 }, //Espresso data set
+    { name: 'Latte', price: 4, quantity: 5 }, //Latte data set
+    { name: 'Cappuccino', price: 4, quantity: 6 }, //Cappuccino data set
+    { name: 'Mocha', price: 5, quantity: 4 } //Mocha data set
+];
+
+console.log(inventory); //Inventory Output
